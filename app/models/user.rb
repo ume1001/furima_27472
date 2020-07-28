7b8c8b7487b0d6a912f620383f11b3477480a1b2
hachi405 <umdryk1001@gmail.com>
2020-07-28 14:42:06 +0900

    add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,23 @@ class User < ApplicationRecord
   has_many :comments
   has_many :item_purchases
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname, uniqueness: true
 
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
 
-  # VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/ }, confirmation: true
+    # VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/ }, confirmation: true
 
-  # VALID_NAME_REGEX =/\A[ぁ-んァ-ン一-龥]/
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # VALID_NAME_REGEX =/\A[ぁ-んァ-ン一-龥]/
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
-  # VALID_KANA_REGEX =/\A[ァ-ヶー－]+\z/
-  validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+    # VALID_KANA_REGEX =/\A[ァ-ヶー－]+\z/
+    validates :first_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
 
-  validates :birth_date, presence: true
+    validates :birth_date
+  end
 end
